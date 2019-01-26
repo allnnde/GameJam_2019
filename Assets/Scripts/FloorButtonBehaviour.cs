a30a49652f7893a76538cccbde5b6bd696ee2c34
unknown <alejandro_beacon@hotmail.com>
2019-01-26 13:15:10 -0300

    Agregado de fuente, comportamiento del boton del suelo

---
+++ Assets/Scripts/FloorButtonBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorButtonBehaviour : MonoBehaviour
+{
+    public GameObject targetObject;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player"){
+            targetObject.GetComponent<DoorBehaviour>().ActivateDoor();
+        }
+    }
+}
