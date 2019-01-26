1f86ed71818cc0db674ef4815987a86fda607bf9
Allende Agustin <agustin_allende@hotmail.com>
2019-01-25 22:35:43 -0300

    Movimiento del personaje

---
+++ Assets/Scripts/PlayerMovimentController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerMovimentController : MonoBehaviour
+{
+    public float VelocidadMovimiento = 6f;
+
+    private Vector3 _forward;
+    private Vector3 _right;
+
+    // Use this for initialization
+    void Start()
+    {
+        _forward = Camera.main.transform.forward;
+        _forward.y = 0;
+        _forward = Vector3.Normalize(_forward);
+        _right = Quaternion.Euler(new Vector3(0, 90, 0)) * _forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mHorizontal = Input.GetAxis("Horizontal");
+        var mVertical = Input.GetAxis("Vertical");
+
+        if (mHorizontal != 0 || mVertical != 0)
+        {
+            Vector3 rightMoviment = _right * VelocidadMovimiento * Time.deltaTime * mHorizontal;
+            Vector3 upMoviment = _forward * VelocidadMovimiento * Time.deltaTime * mVertical;
+
+            var heading = Vector3.Normalize(rightMoviment + upMoviment);
+
+            transform.forward = heading;
+            transform.position += rightMoviment;
+            transform.position += upMoviment;
+        }
+
+    }
+}
