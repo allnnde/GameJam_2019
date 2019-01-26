661918f1cfb1d3b0b9c990aeeadfb5b17600f7af
Allende Agustin <agustin_allende@hotmail.com>
2019-01-25 23:33:10 -0300

    Choque de objetos

---
+++ Assets/Scripts/PushObjectController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PushObjectController : MonoBehaviour
+{
+
+    Rigidbody _rigidbody;
+    public RigidbodyConstraints freesaa;
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody.useGravity = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            _rigidbody.useGravity = true;
+        }
+    }
+
+
+}
