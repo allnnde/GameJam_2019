b2a8d90d39a6875cec0811d7ab5da070b0bdb66c
unknown <alejandro_beacon@hotmail.com>
2019-01-26 00:49:54 -0300

    Palanca y Agregados para la UI

--- Assets/Scripts/sinMovementObject.cs
+++ Assets/Scripts/sinMovementObject.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class sinMovementObject : MonoBehaviour
 {
+    public float amplitude;
+    public float freq;
+    float acumulatedTime = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class sinMovementObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float magnitude = amplitude*Mathf.Sin(freq * acumulatedTime);
+        transform.position += new Vector3(magnitude, 0f,0f);
+        Debug.Log(magnitude);
+        acumulatedTime += Time.deltaTime;
     }
 }
