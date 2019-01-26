ccb502977b762af53115442e8de280d048230edb
Allende Agustin <agustin_allende@hotmail.com>
2019-01-26 13:11:45 -0300

    habilitar del hijo

---
+++ Assets/Scripts/SneakController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SneakController : MonoBehaviour
+{
+    private Animator _anim;
+
+    private void Awake()
+    {
+        _anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            _anim.SetTrigger("UsingSkill");
+
+        }
+    }
+}
