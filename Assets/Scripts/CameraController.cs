1738ffaeb2eb1cf3ddf7eca61d0906799303f3d2
Allende Agustin <agustin_allende@hotmail.com>
2019-01-25 23:32:57 -0300

    Control de camara

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject Target;
+    public float CamaraAnguloY;
+
+    public float smoothing = 5f;
+
+    private Vector3 _offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        EnfocarAlPlayer();
+    }
+
+    public void EnfocarAlPlayer()
+    {
+        transform.rotation = Quaternion.Euler(30, CamaraAnguloY, 0);
+        _offset = transform.position - Target.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 targetCamPos = Target.transform.position + _offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+}
