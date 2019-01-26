780bd9cce43b7f6fef20ef383fce6f7e5b19532f
Allende Agustin <agustin_allende@hotmail.com>
2019-01-26 14:36:26 -0300

    post processing

--- Assets/Scripts/JumpController.cs
+++ Assets/Scripts/JumpController.cs
@@ -1,23 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class JumpController : MonoBehaviour
 {
+    private Animator _anim;
 
     PlayerMovimentController _pmc;
     private void Awake()
     {
         _pmc = GetComponent<PlayerMovimentController>();
+        _anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-              _pmc.Saltar = true;
-            }
-            
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _pmc.Saltar = true;
+            _anim.SetTrigger("UsingSkill");
+        }
+
     }
 }
